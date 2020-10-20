d26f7a4d7ef7263f02e620d38fee0c87eb8012a9
DaniyarTurak <71205067+DaniyarTurak@users.noreply.github.com>
2020-10-20 13:36:08 +0600

    Add files via upload

---
+++ Net_01_08_CreatingTypes/Net_01_08_CreatingTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Net_01_08_CreatingTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Net_01_08_CreatingTypes/Task_1_FindNthRoot/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task_1_FindNthRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 8, 3, 0.1 => 2
+            double number = double.Parse(Console.ReadLine());
+            double a = double.Parse(Console.ReadLine());
+            double precision = double.Parse(Console.ReadLine());
+
+            double count = a;
+            double n = 0;
+            for (int i = 1; i <= Math.Sqrt(number); i++)
+            {
+                double temp_number = number;
+                while (count > 0)
+                {
+                    temp_number /= i;
+                    count--;
+                    n = i;
+                }
+                if (temp_number == 1)
+                    Console.WriteLine(n);
+                else
+                    count = a;
+            }
+        }
+    }
+}
